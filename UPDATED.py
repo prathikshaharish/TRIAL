77060211493e9e9ec4677becea3a9fd968d447fa
PRATHIKSHA HARISH <100825029+prathikshaharish@users.noreply.github.com>
2024-05-30 08:36:40 +0530

    Update UPDATED.py

--- UPDATED.py
+++ UPDATED.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load dataset
-uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+uploaded_file = st.file_uploader("EDITED DATA SET.xlsx", type="xlsx")
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
     st.write("Column names in the dataset:", df.columns.tolist())
